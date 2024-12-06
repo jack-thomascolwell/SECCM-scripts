923ef7573e05dc235d018edfe489545ab0e4128a
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2024-12-06 09:57:50 -0500

    updated

---
+++ sampleHeightCalibration.js
@@ -0,0 +1,5 @@
+const zMin = 7556;
+const fMin = 9418;
+
+const zSample = 10943; // z position of the sample at fMin
+const fSample = 13998; // f position of the sample at zMin
--- tafel.js
+++ tafel.js
@@ -6,7 +6,7 @@ function getCurrent() { // gets current in pA
 }
 
 function constantPotential(bias, duration, delay, filePath) {
-    const f = ['bias (V), current (pA)'];
+    const f = ['time (ms), current (pA)'];
     const v0 = {
         sample: spm.bias.sample,
         tip: spm.bias.tip,
@@ -23,7 +23,7 @@ function constantPotential(bias, duration, delay, filePath) {
     spm.bias.sample = v0.sample;
     spm.bias.tip = v0.tip;
     
-    app.save(f, filePath);
+    app.save(f, filePath); //FIXME: opens new window every time
 }
 
 function lift(distance) {
@@ -39,22 +39,107 @@ function lift(distance) {
     spm.zstage.moveTo(zCoarseTarget);
     spm.sleep(10);
 }
+
+function formCell(threshold, speed, formCellBias, endBias, biasChannel) {
+    const minZstep = 0.01;
+
+    var formedCell = false;
+    var t1 = Date.now();
+    spm.addChannel('current');
+    var t0 = Date.now();
+    var zCoarse0 = spm.zstage.pos;
+    var zFine0 = spm.zscanner.pos;
+
+    print('Forming cell with threshold=' + round(threshold, 3) + 'pA at ' + round(speed, 3) + 'um/s with V' + biasChannel + '=' + round(formCellBias * 1000, 0) + 'mV');
+    if (biasChannel === 'tip') {
+        spm.bias.tip = formCellBias;
+        spm.bias.sample = 0;
+    } else {
+        spm.bias.tip = 0;
+        spm.bias.sample = formCellBias;
+    }
+    while (!formedCell) {
+        var i = getCurrent();
+
+        // step z piezo
+        spm.zscanner.moveTo(spm.zscanner.fullyRetractedPos);
+        spm.sleep(100);
+
+        var zFine = spm.zscanner.pos;
+        var zFineStep = minZstep;
+        var t1 = Date.now();
+        // print('\t' + zFine + ' - ' + zFineStep + ' = ' + (zFine - zFineStep) + ' >= ' + spm.zscanner.fullyExtendedPos + ': ' + (zFine - zFineStep >= spm.zscanner.fullyExtendedPos));
+        while ((zFine - zFineStep >= spm.zscanner.fullyExtendedPos) && (Math.abs(i) < threshold)) { // step z piezo while range is available and current is within threshold
+            spm.zscanner.moveTo(zFine - zFineStep);
+
+            zFine = spm.zscanner.pos;
+            // print('\t\tzFine = ' + zFine + 'um');
+
+            i = getCurrent();
+            // print('\t\ti = ' + i + 'pA');
+
+            const t2 = Date.now();
+            const dt = t2 - t1;
+            t1 = t2;
+            const dz = speed * dt / 1000.0; // target z step based on time passed
+            zFineStep = Math.max(minZstep, dz); // don't step smaller than the minimum z step
+            // print('\t\tzFineStep = ' + zFineStep + 'um');
+
+            if (dz < minZstep) {
+                // print('\t\t delaying by ' + (minZstep / speed * 1000.0) + 'us');
+                spm.sleep(minZstep / speed * 1000.0); // slow down if moving faster than speed
+            }
+        }
+
+        if(Math.abs(i) >= threshold) {
+            formedCell = true;
+            break;
+        }
+
+        spm.zscanner.moveTo(spm.zscanner.fullyRetractedPos);
+        spm.sleep(100);
+
+        const zCoarse = spm.zstage.pos;
+        const zCoarseStep = (spm.zscanner.fullyRetractedPos - spm.zscanner.fullyExtendedPos) * 0.85;
+        spm.zstage.moveTo(zCoarse - zCoarseStep);
+        spm.sleep(100);
+    }
+    
+    if (biasChannel === 'tip') {
+        spm.bias.tip = endBias;
+        spm.bias.sample = 0;
+    } else {
+        spm.bias.tip = 0;
+        spm.bias.sample = endBias;
+    }
+
+    const deltat = Date.now() - t0;
+    const deltaz = zFine0 + zCoarse0 - spm.zscanner.pos - spm.zstage.pos;
+
+    print('Formed cell with i=' + round(i, 3) + 'pA. It took ' + round(deltat / 1000, 0) + 's to travel ' + round(deltaz, 3) + 'um');
+}
+
 const threshold = 3;
 const speed = 0.5;
-const duration = 2000;
+const duration = 90000;
 const delay = 20;
-const pathRoot = 'C:/Users/user/Desktop/JTC/PtGa/tafel-12-11-2024'
+const formCellBias = -0.6;
+const liftHeight = 5;
+const center = [-20,-20]; // center of the point grid
+const size = [0.5, 0.5]; // pixel size
+
+const pathRoot = 'C:/Users/user/Desktop/JTC/Pt/18-11-2024/tafel'
+
 // construct list of potentials
 const potentials = [];
-for (var v = -0.6; v <= -0.4; v+=0.1) potentials.push(v);
-for (var v = 0.4; v <= 1; v+= 0.1) potentials.push(v);
+for (var v = -0.55; v <= -0.44; v+=0.01) potentials.push(v);
+for (var v = 0.55; v <= 0.71; v+= 0.01) potentials.push(v);
+potentials.reverse();
 
 // Tafel Analysis
 
 // construct point grid
 const points = [];
-const center = [0,0]; // center of the point grid
-const size = [0.2, 0.2]; // pixel size
 const pixels = [Math.floor(Math.sqrt(potentials.length)), Math.ceil(potentials.length / Math.floor(Math.sqrt(potentials.length)))];
 if (pixels[0] % 2 === 0) center[0] += size[0] / 2;
 if (pixels[1] % 2 === 0) center[1] += size[1] / 2;
@@ -68,18 +153,24 @@ for (var i = 0; i < potentials.length; i++) {
        y: y,
        v: potentials[i]
     });
-    if (Math.max(Math.abs(x), Math.abs(y) > 50)) throw new Error('xy outside of stage limits');
-    print(JSON.stringify(points[i]))
+    if (Math.max(Math.abs(x), Math.abs(y)) > 50) throw new Error('xy outside of stage limits');
 }
+print('Generated point grid of ' + potentials.length + ' points');
 
 // perform analysis
 for (var i = 0; i < potentials.length; i++) {
     const p = points[i];
+
+    print('Moving to (' + round(p.x, 3) + ', ' + round(p.y, 3) + ')');
+
     lift(liftHeight);
     spm.xyscanner.moveTo(p.x, p.y);
 
     formCell(threshold, speed, formCellBias, 0, 'sample');
-
-    constantPotential(p.v, duration, delay, pathRoot + '_' + Math.round(p.v * 1000) + 'mV');
+    print('Measuring current at Vsample=' + round(p.v*1000, 0) + 'mV');
+    constantPotential(p.v, duration, delay, pathRoot + '/' + round(p.v * 1000, 0) + 'mV.csv');
 }
-print(constantPotential(-0.6, 2000, 10));
+
+function round(v, d) {
+    return Math.round(v * Math.pow(10,d)) / Math.pow(10,d);
+}
