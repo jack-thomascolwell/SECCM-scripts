336a50a581374bef70547d15efbe7edc3714eef1
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2025-10-17 15:50:06 -0400

    Fixed data saving method to log coordinates

--- analysis.py
+++ analysis.py
@@ -1,6 +1,38 @@
 import pspylib as ps
 import numpy as np
 import os
+import re
+
+def extractSmartScriptGrid(dirpath, pattern = r'([0-9]+)[^./\\]*\.tiff', output='combined'):
+    if (not (os.path.exists(dirpath) and os.path.isdir(dirpath))):
+        raise FileNotFoundError()
+    files = dict([(int(re.match(pattern,entry).group(1)), os.path.join(dirpath,entry)) for entry in os.listdir(dirpath) if os.path.isfile(os.path.join(dirpath, entry)) and re.match(pattern ,entry)]  )
+    indices = list(files.keys())
+    indices.sort()
+    data = {}
+    for i in indices:
+        filepath = files[i]
+        if (not os.path.exists(os.path.join(dirpath, filepath))):
+            raise FileExistsError()
+        file = ps.TiffReader(os.path.join(dirpath, filepath))
+
+        data[i] = file.data.spectData.rawData[0,[0,1]] * 1000 # Data (point, (potential / mV, current / pA))
+
+    match output:
+        case 'combined':
+            dataCombined = np.zeros((len(data)+1, max(data[indices[0]][1].shape)))
+            dataCombined[0] = data[indices[0]][0]
+            headerStr = 'potential/mV'
+            for i in indices:
+                headerStr+=f', current_{i+1}/pA'
+                dataCombined[i+1] = data[i][1]
+            print(dataCombined)
+            np.savetxt(os.path.join(dirpath,'combined.csv'), np.transpose(dataCombined), fmt='%.9f', delimiter=', ', header=headerStr)
+        case 'separate':
+            for (i, filepath) in files.items():
+                np.savetxt(os.path.splitext(filepath)[0] + '.csv', np.transpose(data[i]),  fmt='%.9f', delimiter=', ', header='potential/mV, current/pA')
+        case _:
+            raise ValueError()
 
 def extractGrid(filepath, output='combined'):
     if (not os.path.exists(filepath)):
@@ -56,5 +88,5 @@ def extractSingle(filepath):
     outpath = os.path.splitext(filepath)[0] + '.txt'
     np.savetxt(outpath, data,  fmt='%.9f', delimiter=', ', header='potential/mV, current/pA')
 
-path = "C:\\Users\\user\\Desktop\\JTC\\20250902 50mM KCl 1mM RuHex\\prog_88_untreated_CoPi_spot2__003.tiff"
-extractGrid(path)
+path = "C:\\Users\\user\\Desktop\\JTC\\20251016 50mM KCl 1mM RuHex\\test"
+extractSmartScriptGrid(path, output='combined')
